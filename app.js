d580a8927c7239aa41aa3fd7dac7af133d0e845a
JMR-Mejia <chucho4720013@gmail.com>
2021-04-18 00:39:30 -0500

    Computed Properties y Watchers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
   data() {
     return {
       name: "Bitcoin",
+      symbol: "BTC",
       img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
       changePercent: -10,
       color: "f4f4f4",
@@ -19,6 +20,18 @@ new Vue({
       ],
       showPrices: false,
     };
+  },
+
+  computed: {
+    title() {
+      return `${this.name} - ${this.symbol}`;
+    },
+  },
+
+  watch: {
+    showPrices(newVal, oldVal) {
+      console.log(newVal, oldVal);
+    },
   },
 
   methods: {
