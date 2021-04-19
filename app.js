c7b5db33fcb01d26461251b2d1424ab9456057ad
JMR-Mejia <chucho4720013@gmail.com>
2021-04-19 00:20:52 -0500

    Comunicación entre Componentes: propiedades

--- app.js
+++ app.js
@@ -1,44 +1,89 @@
+Vue.component("CoinDetail", {
+  props: ["coin"],
+
+  data() {
+    return {
+      showPrices: false,
+      value: 0,
+    };
+  },
+
+  template: `
+  <div>
+    <img
+      v-on:mouseover="toggleShowPrices"
+      v-on:mouseout="toggleShowPrices"
+      v-bind:src="coin.img"
+      v-bind:alt="coin.name"
+    />
+    <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
+      {{ title }}
+      <span v-if="coin.changePercent > 0">Ok!</span>
+      <span v-else-if="coin.changePercent < 0">Stop</span>
+      <span v-else>Luky!</span>
+
+      <span v-on:click="toggleShowPrices">
+      {{ showPrices ? 'Ocultar precios' : 'Mostrar precios'}}
+      </span>
+    </h1>
+
+    <input type="number" v-model="value" />
+    <span>{{ convertedValue }}</span>
+
+    <ul v-show="showPrices">
+        <li
+          v-bind:class="{ orange: p.value == coin.price, red: p.value < coin.price, green: p.value > coin.price}"
+          v-for="(p, i) in coin.pricesWithDays"
+          v-bind:key="p.day"
+        >
+          {{i}} - {{p.day}} - {{p.value}}
+        </li>
+      </ul>
+  </div>
+  `,
+
+  computed: {
+    title() {
+      return `${this.coin.name} - ${this.coin.symbol}`;
+    },
+
+    convertedValue() {
+      if (!this.value) 0;
+
+      return this.value / this.coin.price;
+    },
+  },
+
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
+    },
+  },
+});
+
 new Vue({
   el: "#app",
 
   data() {
     return {
-      name: "Bitcoin",
-      symbol: "BTC",
-      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-      changePercent: -10,
-      value: 0,
+      btc: {
+        name: "Bitcoin",
+        symbol: "BTC",
+        img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+        changePercent: -10,
+        price: 8400,
+        pricesWithDays: [
+          { day: "Lunes", value: 8400 },
+          { day: "Martes", value: 7900 },
+          { day: "Miercoles", value: 8200 },
+          { day: "Jueves", value: 9000 },
+          { day: "Viernes", value: 9400 },
+          { day: "Sabado", value: 10000 },
+          { day: "Domingo", value: 10200 },
+        ],
+      },
       color: "f4f4f4",
-      price: 8400,
-      pricesWithDays: [
-        { day: "Lunes", value: 8400 },
-        { day: "Martes", value: 7900 },
-        { day: "Miercoles", value: 8200 },
-        { day: "Jueves", value: 9000 },
-        { day: "Viernes", value: 9400 },
-        { day: "Sabado", value: 10000 },
-        { day: "Domingo", value: 10200 },
-      ],
-      showPrices: false,
     };
-  },
-
-  computed: {
-    title() {
-      return `${this.name} - ${this.symbol}`;
-    },
-
-    converterValue() {
-      if (!this.value) 0;
-
-      return this.value / this.price;
-    },
-  },
-
-  watch: {
-    showPrices(newVal, oldVal) {
-      console.log(newVal, oldVal);
-    },
   },
 
   methods: {
