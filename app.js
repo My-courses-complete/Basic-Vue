e0d9c3593d59ec2d44f870e26cf4f314a04b798a
Jesús Mejía <51868559+JMR-Mejia@users.noreply.github.com>
2021-04-21 01:13:55 -0500

    SistemadeComponentes (#7)
    * Comunicación entre Componentes: propiedades
    
    * Comunicación entre Componentes: eventos
    
    * Slots
    
    * Ciclo de Vida y Hooks

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
-new Vue({
-  el: "#app",
+Vue.component("CoinDetail", {
+  props: ["coin"],
 
   data() {
     return {
-      name: "Bitcoin",
-      symbol: "BTC",
-      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-      changePercent: -10,
-      value: 0,
-      color: "f4f4f4",
-      price: 8400,
-      pricesWithDays: [
-        { day: "Lunes", value: 8400 },
-        { day: "Martes", value: 7900 },
-        { day: "Miercoles", value: 8200 },
-        { day: "Jueves", value: 9000 },
-        { day: "Viernes", value: 9400 },
-        { day: "Sabado", value: 10000 },
-        { day: "Domingo", value: 10200 },
-      ],
       showPrices: false,
+      value: 0,
     };
+  },
+
+  template: `
+  <div>
+    <img
+      v-on:mouseover="toggleShowPrices"
+      v-on:mouseout="toggleShowPrices"
+      v-bind:src="coin.img"
+      v-bind:alt="coin.name"
+    />
+    <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
+      {{ title }}
+      <span v-if="coin.changePercent > 0">Ok!</span>
+      <span v-else-if="coin.changePercent < 0">Stop</span>
+      <span v-else>Luky!</span>
+
+      <span v-on:click="toggleShowPrices">
+      {{ showPrices ? 'Ocultar precios' : 'Mostrar precios'}}
+      </span>
+    </h1>
+
+    <input type="number" v-model="value" />
+    <span>{{ convertedValue }}</span>
+
+    <slot name="text"></slot>
+    <slot name="link"></slot>
+
+    <ul v-show="showPrices">
+        <li
+          v-bind:class="{ orange: p.value == coin.price, red: p.value < coin.price, green: p.value > coin.price}"
+          v-for="(p, i) in coin.pricesWithDays"
+          v-bind:key="p.day"
+        >
+          {{i}} - {{p.day}} - {{p.value}}
+        </li>
+      </ul>
+  </div>
+  `,
+
+  created () {
+    console.log('Created CoinDetail...');
+  },
+
+  mounted () {
+    console.log('Mounted CoinDetail...')
   },
 
   computed: {
     title() {
-      return `${this.name} - ${this.symbol}`;
+      return `${this.coin.name} - ${this.coin.symbol}`;
     },
 
-    converterValue() {
+    convertedValue() {
       if (!this.value) 0;
 
-      return this.value / this.price;
-    },
-  },
-
-  watch: {
-    showPrices(newVal, oldVal) {
-      console.log(newVal, oldVal);
+      return this.value / this.coin.price;
     },
   },
 
@@ -45,7 +69,47 @@ new Vue({
     toggleShowPrices() {
       this.showPrices = !this.showPrices;
 
-      this.color = this.color.split("").reverse().join("");
+      this.$emit("change-color",this.showPrices ? "FF96C8" : "3D3D3D");
+    },
+  },
+});
+
+new Vue({
+  el: "#app",
+
+  data() {
+    return {
+      btc: {
+        name: "Bitcoin",
+        symbol: "BTC",
+        img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+        changePercent: -10,
+        price: 8400,
+        pricesWithDays: [
+          { day: "Lunes", value: 8400 },
+          { day: "Martes", value: 7900 },
+          { day: "Miercoles", value: 8200 },
+          { day: "Jueves", value: 9000 },
+          { day: "Viernes", value: 9400 },
+          { day: "Sabado", value: 10000 },
+          { day: "Domingo", value: 10200 },
+        ],
+      },
+      color: "f4f4f4",
+    };
+  },
+
+  created () {
+    console.log('Created...');
+  },
+
+  mounted () {
+    console.log('Mounted...')
+  },
+
+  methods: {
+    updateColor(color) {
+      this.color = color || this.color.split("").reverse().join("");
     },
   },
 });
